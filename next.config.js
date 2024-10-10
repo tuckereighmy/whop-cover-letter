c1e938d35e9ef37ad46060182deb0f40fd132c1c
teighmy22 <89541954+teighmy22@users.noreply.github.com>
2024-10-10 17:59:21 -0400

    Your commit message here

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/whop-cover-letter',
+    assetPrefix: '/whop-cover-letter/',
+  }
+  
+  module.exports = nextConfig
+  
